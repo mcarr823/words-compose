9a0993f5a86cb9af6fa3cbd75b590e9a3899de9e
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-07-20 16:01:44 +1000

    Added some default colors

--- app/src/main/java/dev/mcarr/words/ui/theme/Color.kt
+++ app/src/main/java/dev/mcarr/words/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Orange = Color(0xFFffc107)
+val Blue = Color(0xFF0d6efd)
+val Green = Color(0xFF198754)
+val Gray = Color(0xFF6c757d)
