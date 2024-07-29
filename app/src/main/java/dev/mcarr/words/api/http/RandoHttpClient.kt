a96312b7d2de16940f5116bdd3b7141eaf0f2aef
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-07-29 20:04:56 +1000

    Comments

--- app/src/main/java/dev/mcarr/words/api/http/RandoHttpClient.kt
+++ app/src/main/java/dev/mcarr/words/api/http/RandoHttpClient.kt
@@ -1,5 +1,12 @@
 package dev.mcarr.words.api.http
 
+/**
+ * HTTP client which interfaces with the random word API
+ * hosted on: https://random-word-api.vercel.app
+ *
+ * All requests to the API return a list of strings, even if
+ * only a single word is requested.
+ * */
 class RandoHttpClient : AbstractHttpClient() {
 
     override val baseUrl = "https://random-word-api.vercel.app/"
