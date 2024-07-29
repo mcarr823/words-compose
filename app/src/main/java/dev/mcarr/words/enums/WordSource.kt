ec2e892d608a8e49189f53717b134718a4b00e3c
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-07-29 20:07:27 +1000

    Added an enum for tagging different word sources

---
+++ app/src/main/java/dev/mcarr/words/enums/WordSource.kt
@@ -0,0 +1,17 @@
+package dev.mcarr.words.enums
+
+/**
+ * Represents an endpoint from which this game can retrieve data.
+ *
+ * @param key Unique identifier for the given word source
+ * */
+enum class WordSource(
+    key: String
+) {
+
+    DATABASE("DATABASE"),
+    ONLINE_HEROKU("ONLINE_HEROKU"),
+    ONLINE_RANDO("ONLINE_RANDO"),
+    ONLINE_RYANRK("ONLINE_RYANRK")
+
+}
