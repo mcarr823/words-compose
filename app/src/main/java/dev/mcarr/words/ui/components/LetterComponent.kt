c84f1d01087336def368dc2575fd9450e5fe74a9
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-08-02 19:06:25 +1000

    Added UI components for displaying guessed words on the screen

---
+++ app/src/main/java/dev/mcarr/words/ui/components/LetterComponent.kt
@@ -0,0 +1,68 @@
+package dev.mcarr.words.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.mcarr.words.ui.theme.Typography
+
+/**
+ * Displays a single letter of a guessed word.
+ *
+ * A series of LetterComponents should be displayed
+ * side by side to spell out a word, such as in a
+ * WordComponent.
+ *
+ * @param letter The letter to display
+ * @see WordComponent
+ * */
+@Composable
+fun LetterComponent(
+    letter: String
+) {
+
+    OutlinedCard(
+        modifier = Modifier.padding(2.dp),
+        colors = CardColors(
+            containerColor = Color.White,
+            contentColor = Color.Black,
+            disabledContentColor = Color.Black,
+            disabledContainerColor = Color.White
+        )
+    ) {
+        Text(
+            text = letter,
+            style = Typography.displayLarge,
+            modifier = Modifier.padding(
+                vertical = 8.dp,
+                horizontal = 16.dp
+            )
+        )
+    }
+
+}
+
+@Preview
+@Composable
+fun PreviewLetterComponent(){
+    PreviewComponent {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            LetterComponent(letter = "A")
+            LetterComponent(letter = "B")
+            LetterComponent(letter = "C")
+            LetterComponent(letter = "D")
+            LetterComponent(letter = "E")
+        }
+    }
+}
---
+++ app/src/main/java/dev/mcarr/words/ui/components/WordComponent.kt
@@ -0,0 +1,51 @@
+package dev.mcarr.words.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+/**
+ * Displays a guessed word on the screen.
+ *
+ * Contains a row of LetterComponents and displays
+ * them side by side to spell out a word.
+ *
+ * @param word The word to display
+ * @see LetterComponent
+ * */
+@Composable
+fun WordComponent(
+    word: String
+) {
+
+    val letters = remember {
+        word.chunked(1)
+    }
+
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        letters.forEach {
+            LetterComponent(letter = it)
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun PreviewWordComponent(){
+    PreviewComponent {
+        Column {
+            WordComponent(word = "ABCDE")
+            WordComponent(word = "ABCDE")
+            WordComponent(word = "ABCDE")
+        }
+    }
+}
