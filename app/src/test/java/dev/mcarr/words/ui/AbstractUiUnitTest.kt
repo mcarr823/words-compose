bc507e847e8671eb88c85cad5cbc88e44a255359
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-08-03 15:19:41 +1000

    Added UI test functions for testing multiple component matches

--- app/src/test/java/dev/mcarr/words/ui/AbstractUiUnitTest.kt
+++ app/src/test/java/dev/mcarr/words/ui/AbstractUiUnitTest.kt
@@ -2,7 +2,10 @@ package dev.mcarr.words.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.test.SemanticsNodeInteraction
+import androidx.compose.ui.test.SemanticsNodeInteractionCollection
+import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import org.junit.Rule
@@ -46,6 +49,53 @@ abstract class AbstractUiUnitTest {
     ){
         composeTestRule.onNodeWithText(text).run {
             callback()
+        }
+    }
+
+    /**
+     * Run some unit tests on a group of nodes which display
+     * strings matching the `text` parameter.
+     *
+     * This is used when you want to run tests on the entire
+     * collection, rather than the individual components.
+     *
+     * @param text String used to identify the components
+     * @param callback Block of code to run within the
+     * retrieved component group's context
+     * */
+    fun onAllNodesWithText(
+        text: String,
+        callback: SemanticsNodeInteractionCollection.() -> Unit
+    ){
+        composeTestRule.onAllNodesWithText(text).run {
+            callback()
+        }
+    }
+
+    /**
+     * Run some unit tests on each nodes which displays
+     * a string matching the `text` parameter.
+     *
+     * This is used when you want to run the same set of
+     * tests for each of the nodes which match the `text`
+     * parameter.
+     *
+     * @param text String used to identify the components
+     * @param expectedNumberOfNodes Number of nodes we expect
+     * to be displaying `text`
+     * @param callback Block of code to run within each of
+     * the retrieved components' context
+     * */
+    fun onAllNodesWithText(
+        text: String,
+        expectedNumberOfNodes: Int,
+        callback: SemanticsNodeInteraction.() -> Unit
+    ){
+        onAllNodesWithText(text){
+            assertCountEquals(expectedNumberOfNodes)
+            (0 until expectedNumberOfNodes)
+                .map(this::get)
+                .forEach(callback)
         }
     }
 
