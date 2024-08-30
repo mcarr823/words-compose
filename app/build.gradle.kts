5c4f3db08fb7fde9fd263fdf8b6fe8c3ebf03600
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-08-31 09:22:59 +1000

    Target newest Android SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "dev.mcarr.words"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
