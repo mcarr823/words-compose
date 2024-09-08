5525ce12f15bdd449f261db66376df4ff79147d7
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-09-08 10:30:46 +1000

    Update keyboard hints in a reactive manner

--- app/src/main/java/dev/mcarr/words/ui/components/KeyboardComponent.kt
+++ app/src/main/java/dev/mcarr/words/ui/components/KeyboardComponent.kt
@@ -9,6 +9,10 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,6 +27,13 @@ fun KeyboardComponent(
     modifier: Modifier = Modifier
 ) {
 
+    var hints by remember {
+        mutableStateOf(model.hints)
+    }
+
+    val getHint: (letter: String) -> Hint = { letter ->
+        hints[letter] ?: Hint.NONE
+    }
     val row1 = "QWERTYUIOP".chunked(1)
     val row2 = "ASDFGHJKL".chunked(1)
     val row3 = "ZXCVBNM".chunked(1)
@@ -42,7 +53,7 @@ fun KeyboardComponent(
                 horizontalArrangement = Arrangement.spacedBy(2.dp, Alignment.CenterHorizontally),
             ) {
                 row1.forEach { letter ->
-                    val hint = model.getHint(letter)
+                    val hint = getHint(letter)
                     KeyComponent(letter, hint, width, model::pressKey)
                 }
                 KeyComponent(icon = Icons.AutoMirrored.Default.ArrowBack, width = width, hint = Hint.NONE, onClick = model::backspace)
@@ -52,7 +63,7 @@ fun KeyboardComponent(
                 horizontalArrangement = Arrangement.spacedBy(2.dp, Alignment.CenterHorizontally),
             ) {
                 row2.forEach { letter ->
-                    val hint = model.getHint(letter)
+                    val hint = getHint(letter)
                     KeyComponent(letter, hint, width, model::pressKey)
                 }
                 KeyComponent(icon = Icons.Default.Done, width = width, hint = Hint.NONE, onClick = model::submit)
@@ -62,7 +73,7 @@ fun KeyboardComponent(
                 horizontalArrangement = Arrangement.spacedBy(2.dp, Alignment.CenterHorizontally),
             ) {
                 row3.forEach { letter ->
-                    val hint = model.getHint(letter)
+                    val hint = getHint(letter)
                     KeyComponent(letter, hint, width, model::pressKey)
                 }
             }
