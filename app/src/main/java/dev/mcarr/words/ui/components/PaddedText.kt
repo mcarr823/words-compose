a841f568b7311b43b7ecbf1fb37fe3e9063b0876
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-09-14 15:08:11 +1000

    Padded text component now allows more control over styling

--- app/src/main/java/dev/mcarr/words/ui/components/PaddedText.kt
+++ app/src/main/java/dev/mcarr/words/ui/components/PaddedText.kt
@@ -5,8 +5,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import dev.mcarr.words.ui.theme.Typography
 
 /**
  * Text component with pre-defined padding.
@@ -14,32 +16,73 @@ import androidx.compose.ui.unit.dp
  * Used to provide uniform padding (and other styling later)
  * to all screens.
  *
+ * Padding can optionally be overridden on any side.
+ * eg. In case you want to keep padding uniform on all
+ * sides except one.
+ *
  * @param text Text to display
+ * @param style Style to apply to the text
+ * @param padLeft Padding at the start
+ * @param padRight Padding at the end
+ * @param padTop Padding at the top
+ * @param padBottom Padding at the bottom
  * */
 @Composable
 fun PaddedText(
-    text: String
+    text: String,
+    style: TextStyle = Typography.bodyMedium,
+    padLeft: Int = 8,
+    padRight: Int = 8,
+    padTop: Int = 8,
+    padBottom: Int = 8
 ) {
     Text(
         text = text,
-        modifier = Modifier.padding(8.dp)
+        style = style,
+        modifier = Modifier.padding(
+            start = padLeft.dp,
+            end = padRight.dp,
+            top = padTop.dp,
+            bottom = padBottom.dp
+        )
     )
 }
 
 @Composable
 fun PaddedText(
-    text: AnnotatedString
+    text: AnnotatedString,
+    style: TextStyle = Typography.bodyMedium,
+    padLeft: Int = 8,
+    padRight: Int = 8,
+    padTop: Int = 8,
+    padBottom: Int = 8
 ) {
     Text(
         text = text,
-        modifier = Modifier.padding(8.dp)
+        style = style,
+        modifier = Modifier.padding(
+            start = padLeft.dp,
+            end = padRight.dp,
+            top = padTop.dp,
+            bottom = padBottom.dp
+        )
     )
 }
 
 @Preview
 @Composable
-fun PreviewPaddedText(){
+fun PreviewPaddedTextLight(){
     PreviewComponent {
+        PaddedText(text = "Preview")
+    }
+}
+
+@Preview
+@Composable
+fun PreviewPaddedTextDark(){
+    PreviewComponent(
+        darkMode = true
+    ) {
         PaddedText(text = "Preview")
     }
 }
