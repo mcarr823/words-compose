f98f0f3ac6e164c22a9dfa9ead94ba1c883fa1d6
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-10-07 18:08:44 +1100

    Updated gradle and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.compose.compiler)
-    id("com.google.devtools.ksp") version "2.0.0-1.0.21"
+    id("com.google.devtools.ksp") version "2.0.20-1.0.24"
 }
 
 android {
