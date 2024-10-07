d851eee3786b804be244f3d1fb961cf900f1ca99
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-10-07 18:26:12 +1100

    Version 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.mcarr.words"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
